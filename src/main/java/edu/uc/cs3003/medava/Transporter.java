a30e5de7087bf3fcec8400aeb7515d3393c8a396
shindesc <sarashinde2061@gmail.com>
2024-11-21 17:26:18 -0500

    Update Transporter.java

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,32 @@
 package edu.uc.cs3003.medava;
+
+import java.util.List;
+import java.util.ArrayList;
 public class Transporter {
+
+   // Field to store the name of the transporter
+    private String mTransporterName;
+
+    // Field to store the goods (medicines) being transported
+    public List<Medicine> goods;
+
+    // Constructor to initialize the transporter name
+    public Transporter(String transporterName) {
+        this.mTransporterName = transporterName;
+    }
+
+    // Getter to access the transporter name
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+
+    // Instance initializer to initialize the goods list
+    {
+        goods = new ArrayList<Medicine>();
+    }
+
+    // Method to simulate shipping the goods
+    public void ship() {
+        // Do some shipping!
+    }
 }
