dc6f403c15c9ecbd296d10ff48c928eb907e1b7e
shindesc <sarashinde2061@gmail.com>
2024-11-21 18:29:23 -0500

    Update Hospital.java

--- src/main/java/edu/uc/cs3003/medava/Hospital.java
+++ src/main/java/edu/uc/cs3003/medava/Hospital.java
@@ -1,1 +1,20 @@
 package edu.uc.cs3003.medava;
+
+public class Hospital {
+    private String name;
+
+    public Hospital(String hospitalName) {
+        name = hospitalName;
+    }
+
+    void receive(Transporter t) {
+        while (!t.goods.isEmpty()) {
+            Medicine unloaded = t.goods.remove(0);
+            System.out.println(String.format("Receiving an %s.", unloaded.getMedicineName()));
+        }
+    }
+
+    public String name() {
+        return name;
+    }
+}
