064816a8357384335df108be9e562f2cd7e608ba
shindesc <sarashinde2061@gmail.com>
2024-11-21 18:40:22 -0500

    Update HospitalRunner.java

--- src/main/java/edu/uc/cs3003/medava/HospitalRunner.java
+++ src/main/java/edu/uc/cs3003/medava/HospitalRunner.java
@@ -1,1 +1,15 @@
 package edu.uc.cs3003.medava;
+
+public class HospitalRunner {
+    public static void run() {
+        Transporter semiTruck = new Transporter("SemiTruck");
+
+        Pharmacy cvs = new Pharmacy("CVS at 7500 Beechmont Avenue");
+
+        cvs.send(semiTruck);
+
+        Hospital uc = new Hospital("World Famous University of Cincinnati Children's Hospital");
+
+        uc.receive(semiTruck);
+    }
+}
