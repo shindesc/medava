e5bd4d905c1feca37762aabfc5867afe4aa88dbf
shindesc <sarashinde2061@gmail.com>
2024-11-21 18:31:19 -0500

    Update Pharmacy.java

--- src/main/java/edu/uc/cs3003/medava/Pharmacy.java
+++ src/main/java/edu/uc/cs3003/medava/Pharmacy.java
@@ -1,1 +1,19 @@
 package edu.uc.cs3003.medava;
+
+public class Pharmacy {
+    private String mPharmacyName;
+
+    public Pharmacy(String pharmacyName) {
+        mPharmacyName = pharmacyName;
+    }
+
+    public boolean send(Transporter t) {
+        Medicine advil = new Medicine("Advil");
+        System.out.println(String.format("Sending an %s.", advil.getMedicineName()));
+        return t.goods.add(advil);
+    }
+
+    public String pharmacyName() {
+        return mPharmacyName;
+    }
+}
