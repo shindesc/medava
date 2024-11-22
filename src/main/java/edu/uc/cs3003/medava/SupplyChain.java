cbf4903635169666848813006a3dd7b4c4e71413
shindesc <sarashinde2061@gmail.com>
2024-11-22 10:03:10 -0500

    Update SupplyChain.java

--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -2,7 +2,7 @@ package edu.uc.cs3003.medava;
 
 public class SupplyChain {
     public static void main(String args[]) {
-        
+        Transporter semiTruck = new Transporter("LaserShip", 30.0, 80.0);
 
         HospitalRunner.run();
     }
