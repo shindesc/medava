6e816e0519d31f00712b3eccd1a7696a11d496e1
shindesc <sarashinde2061@gmail.com>
2024-11-22 14:08:26 -0500

    Update Pharmacy.java

--- src/main/java/edu/uc/cs3003/medava/Pharmacy.java
+++ src/main/java/edu/uc/cs3003/medava/Pharmacy.java
@@ -23,6 +23,15 @@ public class Pharmacy {
             System.out.println(String.format("Cannot load %s on to the %s transporter.", activase.getMedicineName(), t.getTransporterName()));
             return false;
         }
+
+        Medicine oxycontin = new Oxycodone();
+        if (t.load(oxycontin)) {
+            System.out.println(String.format("Sending %s on the %s transporter.", oxycontin.getMedicineName(), t.getTransporterName()));
+        } else {
+            System.out.println(String.format("Cannot load %s on to the %s transporter.", oxycontin.getMedicineName(), t.getTransporterName()));
+            return false;
+        }
+
         return true;
     }
 
@@ -30,5 +39,6 @@ public class Pharmacy {
         return mPharmacyName;
     }
 }
+
 
 
