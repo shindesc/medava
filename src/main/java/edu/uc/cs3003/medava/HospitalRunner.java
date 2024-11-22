2590f4194c116392d6ca74e37fd54d9c2d4cca09
shindesc <sarashinde2061@gmail.com>
2024-11-22 10:48:34 -0500

    Update HospitalRunner.java

--- src/main/java/edu/uc/cs3003/medava/HospitalRunner.java
+++ src/main/java/edu/uc/cs3003/medava/HospitalRunner.java
@@ -1,16 +1,17 @@
 package edu.uc.cs3003.medava;
 
 public class HospitalRunner {
-    public static void run() {
-        Transporter semiTruck = new Transporter("LaserShip", 30.0, 80.0);
 
+    public static void run() {
+        Transporter priorityDispatch = new Transporter("Priority Dispatch", 40.0, 41.0);
 
         Pharmacy cvs = new Pharmacy("CVS at 7500 Beechmont Avenue");
 
-        cvs.send(semiTruck);
+        cvs.send(priorityDispatch);
 
         Hospital uc = new Hospital("World Famous University of Cincinnati Children's Hospital");
 
-        uc.receive(semiTruck);
+        uc.receive(priorityDispatch);
     }
 }
+
