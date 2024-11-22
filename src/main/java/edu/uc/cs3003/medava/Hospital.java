7b4e3771fb4e1f5c5b375d2fbe5b8d59aa04ca3c
shindesc <sarashinde2061@gmail.com>
2024-11-22 17:21:07 -0500

    Update Hospital.java

--- src/main/java/edu/uc/cs3003/medava/Hospital.java
+++ src/main/java/edu/uc/cs3003/medava/Hospital.java
@@ -23,5 +23,34 @@ public class Hospital {
     public String getName() {
         return name;
     }
+
+    public boolean send(Transporter t) {
+        Medicine advil = new Ibuprofen();
+        if (t.load(advil)) {
+            System.out.println(String.format("Sending %s on the %s transporter.", advil.getMedicineName(), t.getTransporterName()));
+        } else {
+            System.out.println(String.format("Cannot load %s on to the %s transporter.", advil.getMedicineName(), t.getTransporterName()));
+            return false;
+        }
+
+        Medicine activase = new Thrombolytic();
+        if (t.load(activase)) {
+            System.out.println(String.format("Sending %s on the %s transporter.", activase.getMedicineName(), t.getTransporterName()));
+        } else {
+            System.out.println(String.format("Cannot load %s on to the %s transporter.", activase.getMedicineName(), t.getTransporterName()));
+            return false;
+        }
+
+        Medicine oxycontin = new Oxycodone();
+        if (t.load(oxycontin)) {
+            System.out.println(String.format("Sending %s on the %s transporter.", oxycontin.getMedicineName(), t.getTransporterName()));
+        } else {
+            System.out.println(String.format("Cannot load %s on to the %s transporter.", oxycontin.getMedicineName(), t.getTransporterName()));
+            return false;
+        }
+
+        return true;
+    }
 }
+
 
