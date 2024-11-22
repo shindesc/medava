0b5799cc77ae6b7980b2229234b87cc9ba4f26a7
shindesc <sarashinde2061@gmail.com>
2024-11-22 17:38:16 -0500

    Update Oxycodone.java

--- src/main/java/edu/uc/cs3003/medava/Oxycodone.java
+++ src/main/java/edu/uc/cs3003/medava/Oxycodone.java
@@ -2,22 +2,12 @@ package edu.uc.cs3003.medava;
 
 public class Oxycodone extends Medicine {
 
-    public Oxycodone() {
-        super("Oxycodone");
-    }
+  public Oxycodone() {
+    super("Oxycodone");
+  }
 
-    @Override
-    public MedicineSchedule getSchedule() {
-        return MedicineSchedule.Two;
-    }
-
-    @Override
-    public double minimumTemperature() {
-        return 68.0; // Example temperature
-    }
-
-    @Override
-    public double maximumTemperature() {
-        return 77.0; // Example temperature
-    }
+  @Override
+  public MedicineSchedule getSchedule() {
+    return MedicineSchedule.Two;
+  }
 }
