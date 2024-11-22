ba74d231a4ddc6245dd8a55cea3fd729a0cfb970
shindesc <sarashinde2061@gmail.com>
2024-11-22 13:40:50 -0500

    Update Medicine.java

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,27 +1,86 @@
 package edu.uc.cs3003.medava;
 
-public class Medicine {
-    // Field to store the name of the medicine
+
+
+import java.time.LocalDate;
+
+public abstract class Medicine {
+    private String name;
+    private String manufacturer;
+    private String expirationDate;
+    private int quantity;
     private String mMedicineName;
+
+    // Constructor to initialize all attributes
+    public Medicine(String name, String manufacturer, String expirationDate, int quantity) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.expirationDate = expirationDate;
+        this.quantity = quantity;
+    }
 
     // Constructor to initialize the medicine name
     public Medicine(String medicineName) {
         this.mMedicineName = medicineName;
     }
 
-    // Getter to access the medicine name
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public String getMedicineName() {
         return mMedicineName;
     }
 
     // Method to check if the temperature range is acceptable for the medicine
     public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
-        if (this.minimumTemperature() <= lowTemperature &&
-                highTemperature <= this.maximumTemperature()) {
-            return true;
-        }
-        return false;
+        return this.minimumTemperature() <= lowTemperature && highTemperature <= this.maximumTemperature();
     }
+
+    // Method to check if the medicine is expired
+    public boolean isExpired() {
+        // Assuming expirationDate is in the format "YYYY-MM-DD"
+        LocalDate currentDate = LocalDate.now();
+        LocalDate expDate = LocalDate.parse(expirationDate);
+        return currentDate.isAfter(expDate);
+    }
+
+    // Method to update the quantity
+    public void updateQuantity(int amount) {
+        this.quantity += amount;
+    }
+
+    // Abstract method to get the schedule of the medicine
+    public abstract MedicineSchedule getSchedule();
 
     // Default implementation of minimum temperature
     public double minimumTemperature() {
@@ -32,6 +91,17 @@ public class Medicine {
     public double maximumTemperature() {
         return 100.0;
     }
+
+    @Override
+    public String toString() {
+        return "Medicine{" +
+                "name='" + name + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", expirationDate='" + expirationDate + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
+
 
 
