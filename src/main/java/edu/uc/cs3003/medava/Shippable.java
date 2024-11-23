fbdd26eeed6be79cce6120c10b299930913d467d
shindesc <sarashinde2061@gmail.com>
2024-11-22 22:49:16 -0500

    Update Shippable.java

--- src/main/java/edu/uc/cs3003/medava/Shippable.java
+++ src/main/java/edu/uc/cs3003/medava/Shippable.java
@@ -1,1 +1,7 @@
 package edu.uc.cs3003.medava;
+
+public interface Shippable {
+    MedicineSchedule getSchedule();
+    String getMedicineName();
+    boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature);
+}
