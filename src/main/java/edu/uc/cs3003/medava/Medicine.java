f9c013cfa1295558854a15b8be36e5107c26d18b
shindesc <sarashinde2061@gmail.com>
2024-11-22 22:59:56 -0500

    Update Medicine.java

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,10 +1,8 @@
 package edu.uc.cs3003.medava;
 
-
-
 import java.time.LocalDate;
 
-public abstract class Medicine {
+public abstract class Medicine implements Shippable {
     private String name;
     private String manufacturer;
     private String expirationDate;
@@ -57,11 +55,13 @@ public abstract class Medicine {
         this.quantity = quantity;
     }
 
+    @Override
     public String getMedicineName() {
         return mMedicineName;
     }
 
     // Method to check if the temperature range is acceptable for the medicine
+    @Override
     public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
         return this.minimumTemperature() <= lowTemperature && highTemperature <= this.maximumTemperature();
     }
@@ -80,6 +80,7 @@ public abstract class Medicine {
     }
 
     // Abstract method to get the schedule of the medicine
+    @Override
     public abstract MedicineSchedule getSchedule();
 
     // Default implementation of minimum temperature
@@ -102,6 +103,7 @@ public abstract class Medicine {
                 '}';
     }
 }
+
 
 
 
