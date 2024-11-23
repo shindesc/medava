9e0c493585776bb73f63a2c86e4bc26bf7f2e99a
shindesc <sarashinde2061@gmail.com>
2024-11-22 21:17:27 -0500

    Update Jarvik.java

--- src/main/java/edu/uc/cs3003/medava/Jarvik.java
+++ src/main/java/edu/uc/cs3003/medava/Jarvik.java
@@ -1,27 +1,30 @@
+package edu.uc.cs3003.medava;
+
+// Import the MedicineSchedule class or enum
+import edu.uc.cs3003.medava.MedicineSchedule;
+
 public class Jarvik {
-  Jarvik(String deviceSerialNumber) {
-    serialNumber = deviceSerialNumber;
-  }
+    private String serialNumber;
 
-  public String getMedicineName() {
-    return "Jarvik Artificial Heart";
-  }
-
-  public MedicineSchedule getSchedule() {
-    return MedicineSchedule.Uncontrolled;
-  }
-
-  public String getSerialNumber() {
-    return serialNumber;
-  }
-
-  public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
-    if (30.0 <= lowTemperature && highTemperature <= 90.0) {
-      return true;
+    public Jarvik(String deviceSerialNumber) {
+        serialNumber = deviceSerialNumber;
     }
-    return false;
-  }
 
-  private String serialNumber;
+    public String getMedicineName() {
+        return "Jarvik Artificial Heart";
+    }
+
+    public MedicineSchedule getSchedule() {
+        return MedicineSchedule.Uncontrolled;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        return 30.0 <= lowTemperature && highTemperature <= 90.0;
+    }
 }
+
 
