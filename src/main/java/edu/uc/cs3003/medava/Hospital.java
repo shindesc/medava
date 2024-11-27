10a18dbc1526a7b6746cb650f705a9afb4d9aa6c
shindesc <sarashinde2061@gmail.com>
2024-11-27 14:36:54 -0500

    Update Hospital.java

--- src/main/java/edu/uc/cs3003/medava/Hospital.java
+++ src/main/java/edu/uc/cs3003/medava/Hospital.java
@@ -1,5 +1,8 @@
 package edu.uc.cs3003.medava;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Hospital {
     private String name;
 
@@ -9,23 +12,24 @@ public class Hospital {
 
     void receive(Transporter t) {
         while (!t.isEmpty()) {
-            Object unloaded = t.unload();
+            try {
+                Object unloaded = t.unload();
+                Method getScheduleMethod = unloaded.getClass().getMethod("getSchedule");
+                MedicineSchedule getScheduleMethodResult = (MedicineSchedule) getScheduleMethod.invoke(unloaded);
+                Method getMedicineNameMethod = unloaded.getClass().getMethod("getMedicineName");
+                String getMedicineNameMethodResult = (String) getMedicineNameMethod.invoke(unloaded);
 
-            if (unloaded instanceof Medicine) {
-                Medicine medicine = (Medicine) unloaded;
-                System.out.println(String.format("Checking whether Hospital can receive %s.", medicine.getMedicineName()));
-                if (medicine.getSchedule() != MedicineSchedule.Uncontrolled) {
+                System.out.println(String.format("Checking whether Hospital can receive %s.", getMedicineNameMethodResult));
+                if (getScheduleMethodResult != MedicineSchedule.Uncontrolled) {
                     System.out.println(String.format("Hospital cannot receive controlled substances and %s is a %s.",
-                            medicine.getMedicineName(), medicine.getSchedule().asString()));
+                            getMedicineNameMethodResult, getScheduleMethodResult.asString()));
                 } else {
-                    System.out.println(String.format("Accepted a shipment of %s.", medicine.getMedicineName()));
+                    System.out.println(String.format("Accepted a shipment of %s.", getMedicineNameMethodResult));
                 }
-            } else if (unloaded instanceof Jarvik) {
-                Jarvik device = (Jarvik) unloaded;
-                System.out.println(String.format("Checking whether Hospital can receive %s.", device.getMedicineName()));
-                System.out.println(String.format("Accepted a shipment of %s.", device.getMedicineName()));
-            } else {
-                System.out.println("Unknown item type received. Cannot process.");
+            } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+                    | InvocationTargetException e) {
+                // If reflection fails, assume item cannot be received
+                System.out.println("Item cannot be received due to missing required methods.");
             }
         }
     }
@@ -33,44 +37,8 @@ public class Hospital {
     public String getName() {
         return name;
     }
-
-    public boolean send(Transporter t) {
-        Medicine advil = new Ibuprofen();
-        if (t.load(advil)) {
-            System.out.println(String.format("Sending %s on the %s transporter.", advil.getMedicineName(), t.getTransporterName()));
-        } else {
-            System.out.println(String.format("Cannot load %s on to the %s transporter.", advil.getMedicineName(), t.getTransporterName()));
-            return false;
-        }
-
-        Medicine activase = new Thrombolytic();
-        if (t.load(activase)) {
-            System.out.println(String.format("Sending %s on the %s transporter.", activase.getMedicineName(), t.getTransporterName()));
-        } else {
-            System.out.println(String.format("Cannot load %s on to the %s transporter.", activase.getMedicineName(), t.getTransporterName()));
-            return false;
-        }
-
-        Medicine oxycontin = new Oxycodone();
-        if (t.load(oxycontin)) {
-            System.out.println(String.format("Sending %s on the %s transporter.", oxycontin.getMedicineName(), t.getTransporterName()));
-        } else {
-            System.out.println(String.format("Cannot load %s on to the %s transporter.", oxycontin.getMedicineName(), t.getTransporterName()));
-            return false;
-        }
-
-        // Send a Jarvik device
-        Jarvik heart = new Jarvik("01j9a9lk71");
-        if (t.load(heart)) {
-            System.out.println(String.format("Sending %s on the %s transporter.", heart.getMedicineName(), t.getTransporterName()));
-        } else {
-            System.out.println(String.format("Cannot load %s on to the %s transporter.", heart.getMedicineName(), t.getTransporterName()));
-            return false;
-        }
-
-        return true;
-    }
 }
+
 
 
 
