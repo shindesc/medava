032558bac8f52a178e6f71d43183b3bb9a9c5264
shindesc <sarashinde2061@gmail.com>
2024-11-27 13:40:35 -0500

    Update Jarvik.java

--- src/main/java/edu/uc/cs3003/medava/Jarvik.java
+++ src/main/java/edu/uc/cs3003/medava/Jarvik.java
@@ -1,31 +1,4 @@
+
 package edu.uc.cs3003.medava;
-
-public class Jarvik implements Shippable {
-    private String serialNumber;
-
-    public Jarvik(String deviceSerialNumber) {
-        serialNumber = deviceSerialNumber;
-    }
-
-    @Override
-    public String getMedicineName() {
-        return "Jarvik Artificial Heart";
-    }
-
-    @Override
-    public MedicineSchedule getSchedule() {
-        return MedicineSchedule.Uncontrolled;
-    }
-
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-
-    @Override
-    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
-        return 30.0 <= lowTemperature && highTemperature <= 90.0;
-    }
-}
-
 
 
