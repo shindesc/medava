4338ed99ec06d38efe5d08c970538f1f4ccb319c
shindesc <sarashinde2061@gmail.com>
2024-11-27 13:49:20 -0500

    Update Transporter.java

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -5,11 +5,17 @@ import java.util.ArrayList;
 
 public class Transporter {
 
+    // Field to store the name of the transporter
     private String mTransporterName;
-    private List<Shippable> goods;
+
+    // Field to store the goods (medicines) being transported
+    private List<Medicine> goods;
+
+    // Fields to store the acceptable temperature range
     private double mLowTemperature;
     private double mHighTemperature;
 
+    // Constructor to initialize the transporter name and temperature range
     public Transporter(String transporterName, double lowTemp, double highTemp) {
         this.mTransporterName = transporterName;
         this.mLowTemperature = lowTemp;
@@ -17,26 +23,32 @@ public class Transporter {
         this.goods = new ArrayList<>();
     }
 
+    // Getter to access the transporter name
     public String getTransporterName() {
         return mTransporterName;
     }
 
+    // Method to simulate shipping the goods
     public void ship() {
         // Do some shipping!
     }
 
-    public boolean load(Shippable itemToLoad) {
+    // Method to load a medicine into the transporter
+    public boolean load(Medicine itemToLoad) {
         if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
-            System.out.println(String.format("Sending %s on the %s transporter.", itemToLoad.getMedicineName(), mTransporterName));
-            return goods.add(itemToLoad);
+            System.out.println(String.format("Adding a %s to the transporter.", itemToLoad.getMedicineName()));
+            goods.add(itemToLoad);
+            return true;
         }
         return false;
     }
 
-    public Shippable unload() {
+    // Method to unload a medicine from the transporter
+    public Medicine unload() {
         return goods.isEmpty() ? null : goods.remove(0);
     }
 
+    // Method to check if the transporter is empty
     public boolean isEmpty() {
         return goods.isEmpty();
     }
